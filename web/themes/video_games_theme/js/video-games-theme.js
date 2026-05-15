e4299c7bcbe1766f4bb398709615166da0c2c586
blexontop <fsal453@g.educaand.es>
2026-05-15 11:07:17 +0200

    style(theme): add premium layout for video games catalog and search form

---
+++ web/themes/video_games_theme/js/video-games-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Video Games Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.videoGamesTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
